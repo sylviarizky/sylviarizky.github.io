aaa318b5bc14aadff03ac22c2234eafb20a91455
sylviarizky <54837615+sylviarizky@users.noreply.github.com>
2020-04-27 15:51:12 +0700

    UTS 1.0

---
+++ UTS/index.php
@@ -0,0 +1,277 @@
+<?php  ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand ml-3" href="#"><strong>KueKita</strong></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse ml-5" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link ml-3" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#tradisional">Tradisional</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#kekinian">Kekinian</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#resep">Masukkan Resep</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#us">About Us</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </head>
+  <body>
+    <div class="jumbotron">
+      <h1 class="display-4"><strong>Hello Chef!</strong></h1>
+      <p class="lead">Temukan aneka resep kue tradisional dan kekinian yang kamu cari</p>
+      <hr class="my-3">
+      <a class="btn btn-outline-success btn-lg" href="#" role="button">MULAI</a>
+    </div>
+
+<!-- page 2 -->
+<section id="tradisional">
+  <h2>Kue Tradisional</h1>
+    <div class="row">
+      <div class="card" style="width: 20rem;">
+        <img src="img/kuelapis.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">KUE LAPIS</h5>
+          <p class="card-text">Kue Tradisional yang tak pernah terlewatkan di setiap acara.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/klepon.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">KUE KLEPON</h5>
+          <p class="card-text">Jajanan pasar yang memiliki isi khas, yaitu gula merah yang akan terasa nikmat saat digigit.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/lemper.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">LEMPER</h5>
+          <p class="card-text">Ini nih, kue unik dari beras berisi ayam yang udah disisir terus dibungkus pake daun.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/bikang.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">BIKANG</h5>
+          <p class="card-text">Bentuknya yang unik itu loo yang bikin penasaran gimana rasanya.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/lumpur.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">KUE LUMPUR</h5>
+          <p class="card-text">Jajan pasar yang endol surendol tak kendol kendol.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/apem.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">APEM</h5>
+          <p class="card-text">.......</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/onde.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">ONDE-ONDE</h5>
+          <p class="card-text">.......</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/nagasari.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">NAGASARI</h5>
+          <p class="card-text">.......</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+</section>
+
+<!-- page 3 -->
+<section id="kekinian" style="background-color: #DCDCDC;">
+  <h2>Kue Kekinian</h2>
+    <div class="row">
+      <div class="card" style="width: 20rem;">
+        <img src="img/donat.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">KUE DONAT</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/chocodb.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">CHOCOLATE DESSERT BOX</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/vanillacake.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">VANILLA CAKE</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/piesusu.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">PIE SUSU BALI</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/brownies.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">BROWNIES COKLAT</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/bolukeju.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">BOLU KEJU</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/martabakmanis.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">MARTABAK MANIS MINI</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      <div class="card" style="width: 20rem;">
+        <img src="img/pastry.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">PASTRY</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.</p>
+          <a href="#" class="btn btn-success">MASAK</a>
+        </div>
+      </div>
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+</section>
+
+<!-- page 4 -->
+<section id="resep">
+  <h2>Tulis Resepmu</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="form-group">
+            <label><strong>Nama</strong></label>
+            <input type="text" name="" class="form-control">
+          </div>
+          <div class="form-group">
+            <label><strong>Email</strong></label>
+            <input type="email" name="" class="form-control">
+          </div>
+          <div class="form-group">
+            <label><strong>Bahan</strong></label>
+            <textarea type="text" class="form-control" rows="25"></textarea>
+          </div>
+          <div class="form-group">
+            <label><strong>Cara Masak</strong></label>
+            <textarea type="text" class="form-control" rows="50"></textarea>
+          </div>
+          <button type="submit" class="btn btn-success">KIRIM</button>
+        </div>
+        <div class="col-md-5">
+          <img src="img/cooking.png" width="500px">
+        </div>
+      </div>
+    </div>
+</section>
+
+<!-- page 4 -->
+<section id="us" style="background-color: #DCDCDC;">
+  <h2>About Us</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.
+      </div>
+      <div class="col-md-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet tempor lacus sed aliquam. Maecenas placerat fermentum odio, eu laoreet risus scelerisque eu. Sed sit amet dui augue. Duis dapibus efficitur placerat.
+      </div>
+    </div>
+  </div>
+</section>
+
+<footer class="mt-5 bg-dark text-center">
+  Sylvia Rizky Anissa Noviana - Pemrograman Berbasis Web E 2020
+</footer >
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
